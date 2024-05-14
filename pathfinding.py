3822ff7f18d3261d9b780df13c02946c963bec7b
Teun-Schuurmans <125254095+Teun-Schuurmans@users.noreply.github.com>
2024-05-14 11:00:48 +0200

    added path_finding_algorithm decorator

--- pathfinding.py
+++ pathfinding.py
@@ -1,7 +1,7 @@
-from time import sleep
+from time import sleep, time
 from pygame import display
 from tile import Tile
-from typing import Callable
+from typing import Callable, Any
 
 
 class PathFinder:
@@ -10,18 +10,29 @@ class PathFinder:
         self.algorithms: list[Callable] = [self.dijkstra, self.a_star]
         self.selected_algorithm: Callable = self.algorithms[0]
 
-    def dijkstra(self) -> None:
-        # only runs the algorithm if the start and end tiles are set
-        if self.grid.start and self.grid.end != (-1, -1):
-            for tile in self.grid.tiles:
-                if tile.state == 'start' or 'visited':
-                    neighbours: list[Tile] = []
-                    for neighbour in neighbours:
-                        if neighbour.state == 'empty':
-                            self.visit_tile(neighbour)
+    def path_finding_algorithm(self, function: Callable) -> Callable:
+        def wrapper() -> None:
+            self.clear_visited_tiles()
+            start_time: float = time()
+            function()
+            end_time: float = time()
+            duration: float = end_time - start_time
+            print(f'{function.__name__} took {duration:.2f} seconds')
 
+        return wrapper
+
+    @path_finding_algorithm
+    def dijkstra(self) -> None:
+        pass
+
+    @path_finding_algorithm
     def a_star(self) -> None:
         pass
+
+    def clear_visited_tiles(self) -> None:
+        for tile in self.grid.tiles.values():
+            if tile.state == 'visited':
+                tile.change_state('empty')
 
     def visit_tile(self, tile: Tile) -> None:
         tile.change_state('visited')
